1886da9ff7ddd723c2d67593eee7e17d2b985722
bigcat3997 <vqt.develop@gmail.com>
2026-03-01 21:50:51 +0700

    add new iam base module

---
+++ modules/base/iam/variables.tf
@@ -0,0 +1,46 @@
+variable "name" {
+  description = "The user's name."
+  type        = string
+}
+
+variable "path" {
+  description = "Path in which to create the user."
+  type        = string
+  default     = "/"
+}
+
+variable "permissions_boundary" {
+  description = "The ARN of the policy used to set the permissions boundary for the user."
+  type        = string
+  default     = null
+}
+
+variable "force_destroy" {
+  description = "When true, destroy even if user has non-Terraform-managed access keys, login profile, or MFA devices."
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
+
+variable "create_access_key" {
+  description = "Whether to create an access key for the user created by this module."
+  type        = bool
+  default     = false
+}
+
+variable "attach_policy_arn" {
+  description = "Optional managed policy ARN to attach to the user created by this module."
+  type        = string
+  default     = ""
+}
+
+variable "attach_policy" {
+  description = "Whether to attach a managed policy to the user. This flag is evaluated at plan time and should be provided by the caller."
+  type        = bool
+  default     = false
+}
