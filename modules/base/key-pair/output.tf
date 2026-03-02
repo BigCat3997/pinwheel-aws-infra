9f35f71833f85d9c38117031f697f797bd3746c9
bigcat3997 <vqt.develop@gmail.com>
2026-03-02 22:16:38 +0700

    expose private_key_pem, public_key_openssh as sensitive value

--- modules/base/key-pair/output.tf
+++ modules/base/key-pair/output.tf
@@ -17,3 +17,15 @@ output "fingerprint" {
   description = "The MD5 public key fingerprint"
   value       = try(aws_key_pair.this[0].fingerprint, null)
 }
+
+output "private_key_pem" {
+  description = "The private key in PEM format"
+  value       = tls_private_key.key.private_key_pem
+  sensitive   = true
+}
+
+output "public_key_openssh" {
+  description = "The public key in OpenSSH format"
+  value       = tls_private_key.key.public_key_openssh
+  sensitive   = true
+}
