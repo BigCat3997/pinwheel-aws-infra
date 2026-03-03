7b22e62c74499d889a7d825a4f683c146063835d
bigcat3997 <vqt.develop@gmail.com>
2026-03-03 10:05:31 +0700

    support to attach external ebs volume

--- modules/base/ec2/variables.tf
+++ modules/base/ec2/variables.tf
@@ -56,6 +56,48 @@ variable "volume_delete_on_termination" {
   default = true
 }
 
+variable "create_external_volume" {
+  description = "Whether to create and attach an external EBS volume"
+  type        = bool
+  default     = false
+}
+
+variable "external_volume_size" {
+  description = "Size (GiB) of external EBS volume"
+  type        = number
+  default     = 20
+}
+
+variable "external_volume_type" {
+  description = "Type of external EBS volume"
+  type        = string
+  default     = "gp3"
+}
+
+variable "external_volume_encrypted" {
+  description = "Whether external EBS volume is encrypted"
+  type        = bool
+  default     = false
+}
+
+variable "external_volume_device_name" {
+  description = "Device name for attached external EBS volume"
+  type        = string
+  default     = "/dev/sdf"
+}
+
+variable "external_volume_iops" {
+  description = "Provisioned IOPS for external EBS volume (when supported by volume type)"
+  type        = number
+  default     = null
+}
+
+variable "external_volume_throughput" {
+  description = "Provisioned throughput (MiB/s) for external EBS volume (gp3 only)"
+  type        = number
+  default     = null
+}
+
 variable "metadata_http_endpoint" {
   description = "Whether to enable the instance metadata service"
   type        = string
