408f88424c30eb116aaff367fa8c137c139ab724
bigcat3997 <vqt.develop@gmail.com>
2026-03-03 22:49:09 +0700

    give option to create or get data from exist subnet

--- modules/base/subnet/outputs.tf
+++ modules/base/subnet/outputs.tf
@@ -1,9 +1,19 @@
 output "public_subnet_ids" {
   description = "Map of public subnet names to IDs"
-  value       = { for k, v in aws_subnet.public : k => v.id }
+  value       = var.create ? { for k, v in aws_subnet.public : k => v.id } : { for k, v in data.aws_subnet.public : k => v.id }
 }
 
 output "private_subnet_ids" {
   description = "Map of private subnet names to IDs"
-  value       = { for k, v in aws_subnet.private : k => v.id }
+  value       = var.create ? { for k, v in aws_subnet.private : k => v.id } : { for k, v in data.aws_subnet.private : k => v.id }
+}
+
+output "public_subnets" {
+  description = "Map of public subnet objects"
+  value       = var.create ? aws_subnet.public : data.aws_subnet.public
+}
+
+output "private_subnets" {
+  description = "Map of private subnet objects"
+  value       = var.create ? aws_subnet.private : data.aws_subnet.private
 }
