f4585d6685bd7fdbf96eff16ec2e8655c245f993
bigcat3997 <vqt.develop@gmail.com>
2026-05-16 22:18:16 +0700

    support invoking lambda by ALB

--- modules/base/alb/variables.tf
+++ modules/base/alb/variables.tf
@@ -56,3 +56,68 @@ variable "tags" {
   type    = map(string)
   default = {}
 }
+
+variable "maintenance_mode" {
+  description = "When true, the ALB listener forwards to the lambda maintenance target group instead of the web target group"
+  type        = bool
+  default     = false
+}
+
+variable "lambda_function_arn" {
+  description = "ARN of the Lambda function to register as maintenance target. If set, creates a lambda target group, ALB permission, and target group attachment."
+  type        = string
+  default     = null
+}
+
+variable "lambda_function_name" {
+  description = "Name of the Lambda function (required when lambda_function_arn is set)"
+  type        = string
+  default     = null
+}
+
+variable "lambda_target_group_name" {
+  description = "Name for the lambda maintenance target group (required when lambda_function_arn is set)"
+  type        = string
+  default     = null
+}
+
+variable "health_check_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "health_check_path" {
+  type    = string
+  default = "/"
+}
+
+variable "health_check_protocol" {
+  type    = string
+  default = "HTTP"
+}
+
+variable "health_check_matcher" {
+  description = "HTTP status codes considered healthy (e.g. \"200\" or \"200-399\")"
+  type        = string
+  default     = "200"
+}
+
+variable "health_check_interval" {
+  type    = number
+  default = 30
+}
+
+variable "health_check_timeout" {
+  type    = number
+  default = 5
+}
+
+variable "health_check_healthy_threshold" {
+  type    = number
+  default = 3
+}
+
+variable "health_check_unhealthy_threshold" {
+  type    = number
+  default = 3
+}
