0a1c9b4244f55c8dcabed285dfc6b5e2b50b5288
bigcat3997 <vqt.develop@gmail.com>
2026-06-13 14:29:06 +0700

    remove responsibility of lambda to create role

--- modules/base/lambda/outputs.tf
+++ modules/base/lambda/outputs.tf
@@ -3,24 +3,19 @@ output "id" {
   value       = aws_lambda_function.this.id
 }
 
-output "name" {
-  description = "Name of the Lambda function"
-  value       = aws_lambda_function.this.function_name
-}
-
 output "arn" {
   description = "ARN of the Lambda function"
   value       = aws_lambda_function.this.arn
 }
 
-output "role_name" {
-  description = "IAM role name used by the Lambda function"
-  value       = var.create_role ? aws_iam_role.lambda[0].name : (length(data.aws_iam_role.lambda) > 0 ? data.aws_iam_role.lambda[0].name : null)
+output "name" {
+  description = "Name of the Lambda function"
+  value       = aws_lambda_function.this.function_name
 }
 
 output "role_arn" {
   description = "ARN of the IAM role used by the Lambda function"
-  value       = var.create_role ? aws_iam_role.lambda[0].arn : (length(data.aws_iam_role.lambda) > 0 ? data.aws_iam_role.lambda[0].arn : var.role_arn)
+  value       = var.role_arn
 }
 
 output "function_url" {
