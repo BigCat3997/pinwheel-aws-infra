697e3eecce7f2bf46179e60cc22feb419004dfb1
bigcat3997 <vqt.develop@gmail.com>
2026-06-13 21:03:20 +0700

    change output of subnet

--- modules/base/subnet/outputs.tf
+++ modules/base/subnet/outputs.tf
@@ -1,19 +1,33 @@
-output "public_subnet_ids" {
-  description = "Map of public subnet names to IDs"
-  value       = var.create ? { for k, v in aws_subnet.public : k => v.id } : { for k, v in data.aws_subnet.public : k => v.id }
+output "private_subnet_names" {
+  description = "List of private subnet names"
+
+  value = var.create ? keys(aws_subnet.private) : keys(data.aws_subnet.private)
+}
+
+output "public_subnet_names" {
+  description = "List of public subnet names"
+
+  value = var.create ? keys(aws_subnet.public) : keys(data.aws_subnet.public)
 }
 
 output "private_subnet_ids" {
-  description = "Map of private subnet names to IDs"
-  value       = var.create ? { for k, v in aws_subnet.private : k => v.id } : { for k, v in data.aws_subnet.private : k => v.id }
+  description = "List of private subnet IDs"
+
+  value = var.create ? [for subnet in aws_subnet.private : subnet.id] : [for subnet in data.aws_subnet.private : subnet.id]
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+
+  value = var.create ? [for subnet in aws_subnet.public : subnet.id] : [for subnet in data.aws_subnet.public : subnet.id]
 }
 
 output "public_subnets" {
-  description = "Map of public subnet objects"
-  value       = var.create ? aws_subnet.public : data.aws_subnet.public
+  description = "Map of public subnet"
+  value       = var.create ? { for k, v in aws_subnet.public : k => v.id } : { for k, v in data.aws_subnet.public : k => v.id }
 }
 
 output "private_subnets" {
-  description = "Map of private subnet objects"
-  value       = var.create ? aws_subnet.private : data.aws_subnet.private
+  description = "Map of private subnet"
+  value       = var.create ? { for k, v in aws_subnet.private : k => v.id } : { for k, v in data.aws_subnet.private : k => v.id }
 }
