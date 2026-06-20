644af76eee3ff8751507397e0d38b9679b4f432d
bigcat3997 <vqt.develop@gmail.com>
2026-06-20 16:05:39 +0700

    adapting new syntax

--- modules/impl/eks/main.tf
+++ modules/impl/eks/main.tf
@@ -29,7 +29,7 @@ module "local_nat_gateway" {
   for_each = { for gw in var.nat_gateways : gw.name => gw }
 
   name      = each.value.name
-  subnet_id = module.local_subnet.public_subnet_ids[each.value.subnet_name]
+  subnet_id = module.local_subnet.public_subnets[each.value.subnet_name]
   eip_id    = module.local_eip[each.value.eip_name].id
   tags      = var.tags
 
@@ -48,7 +48,7 @@ module "local_route_table" {
   vpc_id               = module.local_vpc.id
   public_route_tables  = var.public_route_tables
   private_route_tables = var.private_route_tables
-  internet_gateway_id  = module.local_internet_gateway.internet_gateway_id
+  internet_gateway_id  = module.local_internet_gateway.id
   nat_gateway_ids      = { for name, ng in module.local_nat_gateway : name => ng.id }
   tags                 = var.tags
 
@@ -59,8 +59,8 @@ module "local_route_table_association" {
   source                  = "../../base/route-table-association"
   public_rtb_assoc        = var.public_rtb_assoc
   private_rtb_assoc       = var.private_rtb_assoc
-  public_subnet_ids       = module.local_subnet.public_subnet_ids
-  private_subnet_ids      = module.local_subnet.private_subnet_ids
+  public_subnet_ids       = module.local_subnet.public_subnets
+  private_subnet_ids      = module.local_subnet.private_subnets
   public_route_table_ids  = module.local_route_table.public_route_table_ids
   private_route_table_ids = module.local_route_table.private_route_table_ids
 
@@ -126,7 +126,7 @@ module "local_eks" {
   kubernetes_version        = var.eks_kubernetes_version
   cluster_role_arn          = module.local_cluster_role.role_arn
   node_role_arn             = module.local_node_role.role_arn
-  subnet_ids                = values(module.local_subnet.private_subnet_ids)
+  subnet_ids                = values(module.local_subnet.private_subnets)
   security_group_ids        = [module.local_sg_cluster.id]
   endpoint_private_access   = var.endpoint_private_access
   endpoint_public_access    = var.endpoint_public_access
