57a19f186eef1e43b91d9265d7769873a4af2028
bigcat3997 <vqt.develop@gmail.com>
2026-09-06 21:32:34 +0700

    applied use self for sg rule

--- modules/base/sg/main.tf
+++ modules/base/sg/main.tf
@@ -19,6 +19,7 @@ resource "aws_security_group_rule" "ingress" {
   cidr_blocks              = lookup(each.value, "cidr_blocks", null)
   ipv6_cidr_blocks         = lookup(each.value, "ipv6_cidr_blocks", null)
   source_security_group_id = try(each.value.security_group_id, null)
+  self                     = try(each.value.self, null)
   description              = lookup(each.value, "description", null)
 }
 
